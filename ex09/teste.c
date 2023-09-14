580ca3a6d57f247e47e61d70e7c3150f2ec6be2c
Vitor <vitorguilherme286@gmail.com>
2023-09-13 22:34:03 -0300

    ambiente de teste

---
+++ ex09/teste.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main(){
+	setlocale(LC_ALL, "Portuguese");
+	
+	int ano;
+	
+	printf("Insira o ano:\n");
+	scanf("%d", &ano);
+	
+	
+}
+
+
